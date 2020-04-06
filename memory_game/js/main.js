3915d6ba18f60d852117e820625709a3e62b802e
Kaleb Berry <Kash@Kalebs-MBP.attlocal.net>
2020-04-06 18:16:51 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+let cardOne = "King";
+let cardTwo = "Queen";
+let cardThree = "King";
+let cardFour = "Queen";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
