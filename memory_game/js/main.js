8b3d432615a7d4a82e27c914b1908ead024374e8
Kaleb Berry <Kash@Kalebs-MBP.attlocal.net>
2020-04-09 00:12:49 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-console.log("Up and running!");
-let cardOne = "King";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "Queen";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) { 
+} 
+else if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else if (cardsInPlay[0]!== cardsInPlay[1]) {
+	alert("Sorry, try again.");
+}
